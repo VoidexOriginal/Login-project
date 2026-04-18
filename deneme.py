3905fd4636b80c56dfb84023b0eeb7fffd8cd132
VoidexOriginal <bugraangan@gmail.com>
2026-04-19 00:09:03 +0300

    secure update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,14 +1,29 @@
+
+
+
+trytime = 0
+
 # import user database
+
 import users_data
 
+# LİMİTS
+
+while trytime <= 5:
 
 # get input from user
-username = input("username: ")
-password = input("password: ")
+ username = input("username: ")
+
+ password = input("password: ")
 
 
 # validate user credentials
-if username in users_data.users and users_data.users[username] == password:
+ if username in users_data.users and users_data.users[username] == password:
     print("login successful")
-else:
+
+    break
+
+ else:
     print("invalid username or password")
+
+    trytime += 1
